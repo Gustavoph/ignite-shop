99b98540661ab23ab0de4ba1c20bf56c78a849e0
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-12-08 09:34:00 -0300

    feat: adding slider in Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image'
 import type { AppProps } from 'next/app'
 
+import logoImg from '../assets/logo.svg'
+import * as S from '../styles/pages/app';
+import { globalStyles } from '../styles/global'
+
+globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <S.Container>
+      <S.Header>
+        <Image src={logoImg} alt="Logo Ignite Shop" />
+      </S.Header>
+
+      <Component {...pageProps} />
+    </S.Container>
+  )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,61 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main', {
+  width: '100%',
+  display: 'flex',
+  marginLeft: 'auto',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
+  minHeight: 656,
+})
+
+export const Product = styled('a', {
+  background: 'linear-gradient(100deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  cursor: 'pointer',
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  overflow: 'hidden',
+  img: {
+    objectFit: 'cover'
+  },
+
+  '&:hover': {
+    footer: {
+      transform: 'translateY(0%)',
+      opacity: 1
+    }
+  }
+})
+
+export const Footer = styled('footer', {
+  position: 'absolute',
+  bottom: '0.25rem',
+  left: '0.25rem',
+  right: '0.25rem',
+  padding: '2rem',
+
+  borderRadius: 6,
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  background: 'rgba(0, 0, 0, 0.6)',
+
+  strong: {
+    fontSize: '$lg'
+  },
+
+  span: {
+    fontSize: '$xl',
+    fontWeight: 'bold',
+    color: '$green300'
+  },
+
+  transform: 'translateY(110%)',
+  opacity: 0,
+  transition: 'all 0.2s ease-in-out',
+})
